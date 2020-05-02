195d66fea68bff59adb46bd5995810d49e37606a
HUNG <nguyenthaihung07@gmail.com>
2020-05-02 23:28:04 +0800

    test

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "grid";
+}
+
+// keypress left and right for prev and next
+document.addEventListener("keydown", function (event) {
+	var x = event.which || event.keyCode;
+	if (x == 37) plusSlides(-1);
+	if (x == 39) plusSlides(1);
+})
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
